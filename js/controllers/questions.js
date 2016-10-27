57613e6e3261885b15bfad46d4d05068eb4f2c2a
Christina Deemer <cahdeemer@gmail.com>
2016-10-27 13:55:18 -0400

    updated wave mixin, added progress function

--- js/controllers/questions.js
+++ js/controllers/questions.js
@@ -5,12 +5,29 @@ Questions = (function() {
     
     var buildQuestions = function() {
 
-    	$('#quiz-template').tmpl(data.questions[0]).appendTo('#questions');
-    	buildOptions();
+
+
+    	$('#quiz-template').tmpl(data.questions[2]).appendTo('#questions');
+    	
+        buildOptions();
+
+        alert(data.questions[2].step);
+
+        if (data.questions[2].step == 1) {
+            $('#water').attr('data-water-level', 1);
+        } else if (data.questions[2].step == 2) {
+            $('#water').attr('data-water-level', 2);
+        } else if (data.questions[2].step == 3) {
+            $('#water').attr('data-water-level', 3);
+        }
     }
 
     var buildOptions = function() {
     	$('#option-template').tmpl(data.questions[0].options).appendTo('#options');
+    }
+
+    var nextQuestion = function() {
+
     }
 
     var bindEvents = function() {}
